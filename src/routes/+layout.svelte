df2f2bbaad797fb713bb98a08507ffef8859fe1c
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-09 02:03:23 +0700

    done temporary footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import MainNavbar from "../component/MainNavbar.svelte";
+	import MainFooter from '../component/MainFooter.svelte';
 	let { children } = $props();
 </script>
 
+<MainNavbar/>
 {@render children()}
+<MainFooter/>
