e297897efc36d8bfca1ad31ac0542625fb3d391c
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-08 23:56:17 +0700

    create navbar component

---
+++ src/component/MainNavbar.svelte
@@ -0,0 +1,24 @@
+<script>
+  // @ts-nocheck
+  
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+</script>
+
+  
+
+<Navbar class="bg-blue-950">
+	<NavBrand href="/">
+	  <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+	</NavBrand>
+	<NavHamburger  />
+	<NavUl >
+	  <NavLi href="/">Home</NavLi>
+	  <NavLi href="#">Event</NavLi>
+	  <NavLi href="#">Learning</NavLi>
+	  <NavLi href="#">Research</NavLi>
+	  <NavLi href="#">Investment Club</NavLi>
+		<NavLi href="#">About</NavLi>
+	</NavUl>
+  </Navbar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,12 @@
 <script>
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+// @ts-nocheck
 	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, FooterBrand, FooterIcon } from 'flowbite-svelte';
-	import { FacebookSolid, GithubSolid, DiscordSolid, TwitterSolid } from 'flowbite-svelte-icons';
+  import { FacebookSolid, GithubSolid, DiscordSolid, TwitterSolid } from 'flowbite-svelte-icons';
+  
+	import MainNavbar from "../component/MainNavbar.svelte";
+</script>
 
-  </script>
-
-<Navbar class="bg-blue-950">
-	<NavBrand href="/">
-	  <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
-	</NavBrand>
-	<NavHamburger  />
-	<NavUl >
-	  <NavLi href="/">Home</NavLi>
-	  <NavLi href="/about">About</NavLi>
-	  <NavLi href="/docs/components/navbar">Navbar</NavLi>
-	  <NavLi href="/pricing">Pricing</NavLi>
-	  <NavLi href="/contact">Contact</NavLi>
-	</NavUl>
-  </Navbar>
+<MainNavbar/>
 
   
   <Footer footerType="socialmedia">
