2b2bbf9e9beae4feece33a12807d87172a45801b
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-09 11:33:46 +0700

    improve footer and upper section

--- src/component/MainFooter.svelte
+++ src/component/MainFooter.svelte
@@ -1,15 +1,70 @@
 <script>
 	// @ts-nocheck
 	import { FacebookSolid, InstagramSolid, WhatsappSolid, LinkedinSolid, MessageCaptionOutline, PhoneOutline } from 'flowbite-svelte-icons';
-	</script>
-	
-	<footer class="bg-[#222831] text-white text-end px-12 py-8 mt-8 relative w-full">
+</script>
+
+<div class="flex flex-col min-h-screen w-full">
+	<main class="flex-grow">
+	</main>
+
+	<footer class="bg-[#222831] text-white px-12 py-8 w-full">
 	  <div class="grid grid-cols-3">
 		<div class="flex flex-col items-center justify-self-start">
 		  <div>
 			<img src="/images/logo_usc_full_white.svg" alt="USC" class="w-32 h-auto"/>
 		  </div>
 		  <!-- Ikon Sosial Media -->
+		  <div class="flex justify-center space-x-4 mt-2">
+			<a href="/#" target="_blank" class="hover:text-gray-300">
+			  <FacebookSolid class="w-6 h-6" />
+			</a>
+			<a href="/#" target="_blank" class="hover:text-gray-300">
+			  <InstagramSolid class="w-6 h-6" />
+			</a>
+			<a href="/#" target="_blank" class="hover:text-gray-300">
+			  <WhatsappSolid class="w-6 h-6" />
+			</a>
+			<a href="/#" target="_blank" class="hover:text-gray-300">
+			  <LinkedinSolid class="w-6 h-6" />
+			</a>
+		  </div>
+		</div>
+		
+		<div class="text-xl justify-self-end w-52">
+		  <p class="text-center font-bold">Contact Info</p>
+		  <div>
+			<div class="flex items-center text-xs text-left space-x-2">
+			  <MessageCaptionOutline class="w-6 h-6" />
+			  <p>+62 123-456-789 Lorem Ipsum</p>
+			</div>
+			<div class="flex items-center text-xs text-left space-x-2">
+			  <PhoneOutline class="w-6 h-6" />
+			  <p>loremipsum@gmail.com</p>
+			</div>
+		  </div>
+		</div>
+		
+		<div class="text-xl justify-self-end w-32">
+		  <p class="text-center font-bold">Address</p>
+		  <p class="text-xs text-left mt-4">Lorem ipsum dolor sit amet consectetur. Amet augue amet massa varius purus faucibus. Sagittis netus egestas eu eget justo euismod sit</p>
+		</div>
+	  </div>
+      
+      <!-- Separate div for copyright with centered text -->
+      <div class="text-center mt-4">
+        <p  class=" text-xs">Copyright {new Date().getFullYear()} &copy; UGM Stock Club</p>
+      </div>
+	</footer>
+</div>
+
+<!-- 	
+	<footer class="bg-[#222831] text-white text-end px-12 py-8 mt-8 fixed bottom-0 w-full">
+	  <div class="grid grid-cols-3">
+		<div class="flex flex-col items-center justify-self-start">
+		  <div>
+			<img src="/images/logo_usc_full_white.svg" alt="USC" class="w-32 h-auto"/>
+		  </div>
+		  
 		  <div class="flex justify-center space-x-4 mt-2">
 			<a href="/#" target="_blank" class="hover:text-gray-300">
 			  <FacebookSolid class="w-6 h-6" />
@@ -45,5 +100,5 @@
 		<p>Copyright <span id="year"></span> &copy; UGM Stock Club</p>
 	  </div>
 	</footer>
-	
+	 -->
 	
--- src/component/UpperSection.svelte
+++ src/component/UpperSection.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">import { Section, ContentWithImage } from "flowbite-svelte-blocks";
 </script>
 
-<div class="p-8">
-	<!-- Title Section -->
-	<h1 class="text-9xl font-Montserrat font-bold">
-	  <span class="text-[#1d5d78]">UGM</span> 
-	  <span class="text-[#1D5C78]">Stock</span> 
-	  <span class="bg-gradient-to-b from-[#1b4d62] to-[#88b050] text-transparent bg-clip-text">Club</span>
-	</h1>
+<div class="p-[2rem] grid grid-cols-[3fr_1fr]">
+	<div class="">
+		<h1 class="text-9xl font-Montserrat font-bold">
+			<span class="text-[#1d5d78]">UGM</span> 
+			<span class="text-[#1D5C78]">Stock</span> 
+			<span class="bg-gradient-to-b from-[#1b4d62] to-[#88b050] text-transparent bg-clip-text">Club</span>
+		</h1>
+	</div>
   
-	<!-- Description Section -->
-	<p class="mt-18 text-lg text-gray-700 max-w-2xl font-Roboto">
-	  A Student Activity Unit located at Gadjah Mada University and focuses on learning and developing oneself in the field of capital markets and investment through various educational activities.
-	</p>
-	<div class="float-right">
-		<img src="/images/arrow.png" alt="Arrow" class="w-32 h-auto"/>
+	<div class="flex justify-center ">
+		<img src="/images/arrow_green.svg" alt="Arrow" class="w-32 h-auto"/>
+	</div>
+
+	<div class=" w-[60%] ">
+		<p class="mt-[2rem] text-lg text-gray-700 max-w-2xl font-Roboto">
+			A Student Activity Unit located at Gadjah Mada University and focuses on learning and developing oneself in the field of capital markets and investment through various educational activities.
+		</p>
 	</div>
 </div>
+
