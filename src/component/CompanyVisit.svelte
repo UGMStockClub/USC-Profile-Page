42a42fc9a80dc66a02c31046246136008759f318
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-09 16:00:35 +0700

    add button to company visit

--- src/component/CompanyVisit.svelte
+++ src/component/CompanyVisit.svelte
@@ -1,35 +1,18 @@
 <script>
+  import CompanyVisitCard from "./CompanyVisitCard.svelte";
 
 </script>
 
 <div class="flex flex-col gap-[2rem] mt-[2rem]">
-
-  <div class="mx-[2rem] bg-red-100 rounded-lg overflow-hidden relative 
-              after:content-[''] after:absolute after:inset-0 after:bg-black/50 
-      after:transition after:duration-300 hover:after:bg-black/20 group">
-    <img src="/images/company_visit1.png" alt="company visit 2" class=""/>
-    <div class="absolute inset-0  flex items-center gap-[2rem] group-hover:opacity-25">
-      <p class="font-bold text-white text-right text-7xl  z-10 w-1/2">
-        Company Visit 1
-      </p>
-      <p class=" text-white text-sm z-10 w-1/2 ">
-        Lorem ipsum dolor sit amet consectetur. Libero enim in viverra non. Et tortor egestas quam malesuada. Gravida orci turpis mauris commodo dolor. Nulla consectetur id sit velit at amet id nec. Tristique ut vehicula pellentesque et sagittis enim varius elementum. Diam et cursus arcu diam dignissim cras aliquam. Nunc in bibendum eget turpis ut diam duis semper. 
-      </p>
-    </div>
-  </div>
-
-  <div class="mx-[2rem] bg-red-100 rounded-lg overflow-hidden relative 
-              after:content-[''] after:absolute after:inset-0 after:bg-black/50 
-      after:transition after:duration-300 hover:after:bg-black/20 group">
-    <img src="/images/company_visit2.png" alt="company visit 2" class=""/>
-    <div class="absolute inset-0  flex items-center gap-[2rem] group-hover:opacity-25">
-      <p class="font-bold text-white text-right text-7xl  z-10 w-1/2">
-        Company Visit 2
-      </p>
-      <p class=" text-white text-sm z-10 w-1/2 ">
-        Lorem ipsum dolor sit amet consectetur. Libero enim in viverra non. Et tortor egestas quam malesuada. Gravida orci turpis mauris commodo dolor. Nulla consectetur id sit velit at amet id nec. Tristique ut vehicula pellentesque et sagittis enim varius elementum. Diam et cursus arcu diam dignissim cras aliquam. Nunc in bibendum eget turpis ut diam duis semper. 
-      </p>
-    </div>
-  </div>
-
+  
+  <CompanyVisitCard title={"Company Visit 1"} 
+                    description = {"Lorem ipsum dolor sit amet consectetur. Libero enim in viverra non. Et tortor egestas quam malesuada. Gravida orci turpis mauris commodo dolor. Nulla consectetur id sit velit at amet id nec. Tristique ut vehicula pellentesque et sagittis enim varius elementum. Diam et cursus arcu diam dignissim cras aliquam. Nunc in bibendum eget turpis ut diam duis semper. "}
+                    img_dir = {"/images/company_visit1.png"}
+  />
+  
+  <CompanyVisitCard title={"Company Visit 2"} 
+                    description = {"Lorem ipsum dolor sit amet consectetur. Libero enim in viverra non. Et tortor egestas quam malesuada. Gravida orci turpis mauris commodo dolor. Nulla consectetur id sit velit at amet id nec. Tristique ut vehicula pellentesque et sagittis enim varius elementum. Diam et cursus arcu diam dignissim cras aliquam. Nunc in bibendum eget turpis ut diam duis semper. "}
+                    img_dir = {"/images/company_visit2.png"}
+  />
+  
 </div>
---
+++ src/component/CompanyVisitCard.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Button } from 'flowbite-svelte';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+  export let title, description, img_dir;
+// console.log(title)
+</script>
+
+<div class="mx-[3rem] bg-red-100 rounded-4xl overflow-hidden relative 
+              after:content-[''] after:absolute after:inset-0 after:bg-black/50 
+    after:transition after:duration-300 hover:after:bg-black/20 group">
+  <img src={img_dir} alt={title} class="z-0"/>
+  <div class="absolute inset-0  flex items-center gap-[2rem] group-hover:opacity-25">
+    <p class="font-bold text-white text-right text-7xl  z-10 w-1/2">{title}</p>
+    <p class=" text-white text-sm z-10 w-1/2 ">
+      {description}
+    </p>
+  </div>
+
+  <div class="absolute right-[2rem] bottom-[1.5rem] opacity-25 group-hover:opacity-100 duration-300
+              pointer-events-auto z-20 cursor-pointer">
+      <Button class="bg-[#88b050] hover:bg-[#1b4d62]">
+        Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+      </Button>
+  </div>
+</div>
