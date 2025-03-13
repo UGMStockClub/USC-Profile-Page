68e79348e60c91bbf5191ad24ea37eb4da4a7c31
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-13 19:55:03 +0700

    fix footer in about page

--- src/component/MainFooter.svelte
+++ src/component/MainFooter.svelte
@@ -3,51 +3,52 @@
 	import { FacebookSolid, InstagramSolid, WhatsappSolid, LinkedinSolid, MessageCaptionOutline, PhoneOutline } from 'flowbite-svelte-icons';
 </script>
 
-<footer class=" w-full bg-[#222831] text-white px-12 py-8 w-full">
+<footer class="bg-[#222831] text-white px-[4rem] py-[3rem] w-full">
 	<div class="grid grid-cols-3">
 		<div class="flex flex-col items-center justify-self-start">
 			<div>
-			<img src="/images/logo_usc_full_white.svg" alt="USC" class="w-32 h-auto"/>
+				<img src="/images/logo_usc_full_white.svg" alt="USC" class="w-32 h-auto"/>
 			</div>
 			<!-- Ikon Sosial Media -->
 			<div class="flex justify-center space-x-4 mt-[1rem]">
-			<a href="/#" target="_blank" class="hover:text-gray-300">
-				<FacebookSolid class="w-6 h-6" />
-			</a>
-			<a href="/#" target="_blank" class="hover:text-gray-300">
-				<InstagramSolid class="w-6 h-6" />
-			</a>
-			<a href="/#" target="_blank" class="hover:text-gray-300">
-				<WhatsappSolid class="w-6 h-6" />
-			</a>
-			<a href="/#" target="_blank" class="hover:text-gray-300">
-				<LinkedinSolid class="w-6 h-6" />
-			</a>
+				<a href="/#" target="_blank" class="hover:text-gray-300">
+					<FacebookSolid class="w-6 h-6" />
+				</a>
+				<a href="/#" target="_blank" class="hover:text-gray-300">
+					<InstagramSolid class="w-6 h-6" />
+				</a>
+				<a href="/#" target="_blank" class="hover:text-gray-300">
+					<WhatsappSolid class="w-6 h-6" />
+				</a>
+				<a href="/#" target="_blank" class="hover:text-gray-300">
+					<LinkedinSolid class="w-6 h-6" />
+				</a>
 			</div>
 		</div>
 	
-	<div class="text-xl justify-self-end w-56">
-		<p class="ml-[1rem] font-bold">Contact Info</p>
-		<div class=" mt-2 ml-3">
-		<div class="flex items-center text-xs text-left space-x-2">
-			<MessageCaptionOutline class="w-6 h-6" />
-			<p>+62 123-456-789 Lorem Ipsum</p>
+		<div class="text-xl justify-self-end w-56">
+			<p class="ml-[1rem] font-bold">Contact Info</p>
+			<div class=" mt-2 ml-3">
+			<div class="flex items-center text-xs text-left space-x-2">
+				<MessageCaptionOutline class="w-6 h-6" />
+				<p>+62 123-456-789 Lorem Ipsum</p>
+			</div>
+
+			<div class="flex items-center text-xs text-left space-x-2">
+				<PhoneOutline class="w-6 h-6" />
+				<p>loremipsum@gmail.com</p>
+			</div>
+			</div>
 		</div>
-		<div class="flex items-center text-xs text-left space-x-2">
-			<PhoneOutline class="w-6 h-6" />
-			<p>loremipsum@gmail.com</p>
+		
+		<div class="text-xl justify-self-end w-32 mr-[1rem]">
+			<p class="text-center font-bold">Address</p>
+			<p class="text-xs text-left  mt-2">Lorem ipsum dolor sit amet consectetur. Amet augue amet massa varius purus faucibus. Sagittis netus egestas eu eget justo euismod sit</p>
 		</div>
-		</div>
-	</div>
-	
-	<div class="text-xl justify-self-end w-32 mr-[1rem]">
-		<p class="text-center font-bold">Address</p>
-		<p class="text-xs text-left  mt-2">Lorem ipsum dolor sit amet consectetur. Amet augue amet massa varius purus faucibus. Sagittis netus egestas eu eget justo euismod sit</p>
-	</div>
 	</div>
 		
 		<!-- Separate div for copyright with centered text -->
-		<div class="text-center mt-[2rem] ">
+		<div class="text-center mt-[2rem]">
 			<p  class=" text-xs">Copyright {new Date().getFullYear()} &copy; UGM Stock Club</p>
 		</div>
 </footer>
