55223f3ee49c1453c9619e741d47e7792acec856
david <davidneilleenirvinne@mail.ugm.ac.id>
2025-03-18 13:48:53 +0700

    add visi misi

--- src/lib/components/WhoAreWe.svelte
+++ src/lib/components/WhoAreWe.svelte
@@ -7,19 +7,23 @@
 
 </script>
 
-<div class="bg-[#143D60] p-[1.2rem] lg:p-[2rem] rounded-4xl ">
+<div class="bg-[#143D60] p-[1.2rem] lg:p-[2rem] rounded-4xl shadow-xl">
 
-  <div class="flex flex-col lg:flex-row lg:justify-center gap-[2rem] lg:gap-[5rem] ">
+  <div class="flex flex-col xl:flex-row xl:justify-center xl:gap-x-[2rem] gap-y-[1rem]">
 
-    <div class="lg:w-[40%] ">
-      <h2 class="text-[#88b050] font-bold text-7xl mb-2">Who are we?</h2>
+    <div class="xl:w-[40%] h-auto">
+      <div class="text-[#88b050] font-bold text-3xl sm:text-5xl lg:text-7xl mb-2 flex flex-row gap-x-[1rem] justify-center ">
+        <p>Who are </p>
+        <p class="text-slate-200">we?</p>
+      </div>
 
-      <p class="text-white text-justify">
+      <p class="text-white text-justify text-sm lg:text-lg">
         Lorem ipsum dolor sit amet consectetur. Libero enim in viverra non. Et tortor egestas quam malesuada. Gravida orci turpis mauris commodo dolor. Nulla consectetur id sit velit at amet id nec. Tristique ut vehicula pellentesque et sagittis enim varius elementum. Diam et cursus arcu diam dignissim cras aliquam. Nunc in bibendum eget turpis ut diam duis semper. Arcu enim ipsum commodo nec vestibulum massa. Proin purus cras diam integer enim quis. Sollicitudin amet vel morbi euismod gravida et semper neque. Sed nisi mauris imperdiet lacus sapien in.
       </p>
+
     </div>
     <!-- svelte-ignore a11y_img_redundant_alt -->
-    <img src="/images/usc_members.png"  alt="" class="lg:w-[55%] h-auto rounded-4xl"/>
+    <img src="/images/usc_members.png"  alt="" class="xl:w-[55%] h-auto rounded-4xl "/>
 
   </div>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,24 @@
 <script>
   import WhoAreWe from "$lib/components/WhoAreWe.svelte";  
+  import TextCard from "$lib/components/TextCard.svelte";
 
   console.log("about page loaded!");
 </script>
 
-<div class="mx-[1rem] lg:mx-[3rem] ">
+<div class="mx-[1rem] lg:mx-[3rem] flex flex-col gap-y-[4rem]">
   <WhoAreWe/>
+
+  <div class="flex flex-col md:flex-row gap-[1.5rem]">
+    <TextCard title="Our Vision" description={["UGM Stock Club sebagai wadah yang dinamis-progresif untuk belajar pasar modal bagi mahasiswa/i agar dapat memberikan impact bagi masyarakat luas dalam memahami dinamika pasar modal."]}/>
+    <TextCard title="Our Mission" 
+    description={[
+       "Meningkatkan literasi pasar modal",
+       "Membangun branding positif UGM Stock Club dengan kegiatan bermanfaat baik dalam lingkup internal dan eksternal",
+       "Memberikan fasilitas pembelajaran pasar modal bagi mahasiswa UGM",
+       "Mendorong partisipasi anggota dan mahasiswa UGM dalam kompetisi dan kegiatan pasar modal",
+       "Membangun hubungan baik dalam lingkup internal dan eksternal"
+       ]}
+      />
+  </div>
+  
 </div>
